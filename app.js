3c3bc8f55a532e02a52ae1747e78235f0c88f871
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-05-05 14:35:17 +0530

    template engine setup

--- app.js
+++ app.js
@@ -11,10 +11,24 @@ var port=process.env.PORT ||5000;
 
 //to use static files
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
+app.set('views','./src/views');
+//app.set('view engine','jade');  // setting jade view engine
+
+// var handlebars= require('express-handlebars');
+// app.engine('.hbs',handlebars({extname:'.hbs'}));
+
+// app.set('view engine','.hbs');  // setting hbs view engine
+
+app.set('view engine','ejs');  // setting ejs view engine
+
+// app.get('/', function (req,resp) {
+//     resp.send('welcome to my express api');
+
+// });
 
 app.get('/', function (req,resp) {
-    resp.send('welcome to my express api');
+    resp.render('index',{title:"Hello from render",list:['a','b']}); // list passed to view
 
 });
 
